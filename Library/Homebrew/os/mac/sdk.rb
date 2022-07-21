0548ef5ac1bc23120418d9760fe4e29d0e28a8dc
Chris Jones <chris.r.jones.1983@gmail.com>
2022-07-21 16:25:14 -0500

    hack `sdk.rb` file so more

--- Library/Homebrew/os/mac/sdk.rb
+++ Library/Homebrew/os/mac/sdk.rb
@@ -77,7 +77,8 @@ module OS
 
         # Use unversioned SDK only if we don't have one matching that version.
         sdk_path = Pathname.new("#{sdk_prefix}/MacOSX.sdk")
-        if (version = read_sdk_version(sdk_path)) && found_versions.exclude?(version)
+        # NOTE: ipatch, attempt to avoid exclude
+        if (version = read_sdk_version(sdk_path)) # && found_versions.exclude?(version)
           @all_sdks << SDK.new(version, sdk_path, source)
         end
 
