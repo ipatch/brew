47b0d3b2b9e1d139c4753ba1a6e3b115ed4e07df
Chris Jones <chris.r.jones.1983@gmail.com>
2022-07-21 12:15:00 -0500

    add additional note

--- Library/Homebrew/formula.rb
+++ Library/Homebrew/formula.rb
@@ -1501,11 +1501,14 @@ class Formula
   # NOTE: ipatch, makde building with debug symbols standard
   # NOTE: ipatch, set min macos target for all builds to 10.14 SDK
 
+  # NOTE: ipatch, ideally setting a env var for a specific os deployment target would be more robust
+
   def std_cmake_args(install_prefix: prefix, install_libdir: "lib", find_framework: "LAST")
     args = %W[
       -DCMAKE_INSTALL_PREFIX=#{install_prefix}
       -DCMAKE_INSTALL_LIBDIR=#{install_libdir}
       -DCMAKE_BUILD_TYPE=Debug
+      -DMACOSX_DEPLOYMENT_TARGET="10.14"
       -DCMAKE_FIND_FRAMEWORK=#{find_framework}
       -DCMAKE_VERBOSE_MAKEFILE=ON
       -Wno-dev
