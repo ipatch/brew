2f88658010b322e0fb337cd2c2fd860b9b905037
chris <chris.r.jones.1983@gmail.com>
2025-04-26 18:49:06 -0500

    modify formula.rb to have cmake use HOMEBREW_MAKE_JOBS

--- Library/Homebrew/formula.rb
+++ Library/Homebrew/formula.rb
@@ -1772,6 +1772,7 @@ class Formula
       -DCMAKE_PROJECT_TOP_LEVEL_INCLUDES=#{HOMEBREW_LIBRARY_PATH}/cmake/trap_fetchcontent_provider.cmake
       -Wno-dev
       -DBUILD_TESTING=OFF
+      -DCMAKE_BUILD_PARALLEL_LEVEL=#{ENV.make_jobs}
     ]
   end
 
